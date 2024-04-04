a4dd76c2d7006d40a630b62d97d08e35f41b587c
readi2play <r.dippe@rene-dippe.de>
2024-04-05 00:38:16 +0200

    Minor Fix to color.Get helper function to make error message more valuable

--- Helpers/color.lua
+++ Helpers/color.lua
@@ -5,7 +5,7 @@ READI.Helper.color = READI.Helper.color or {}
 
 function READI.Helper.color:Get(name, table, ...)
   table = table or READI.Colors
-  if not table[name] then error("Requested color could not be found within color tables.") end
+  if not table[name] then error("Requested color could not be found within color tables.", 2) end
   if select("#", ...) > 0 then
     return format("|cFF%s%s|r", table[name], select(1, ...))
   end 
