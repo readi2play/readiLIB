48f13ea6105d879d51d9bc73a097bb6358bcc066
readi2play <r.dippe@rene-dippe.de>
2024-04-05 00:10:24 +0200

    ## NEW FEATURES
    - implemented utility factory function for checkboxes
    - implemented a color table for commonly used colors and corporate id colors
    - implement color:Get helper function to return and format a string with a given color
    - fixed a minor code smell in the button utility factory function
    - fixed typo in string:Capitalize helper function
    - fixed error in string:Capitalized helper function where the given string was not formatted correctly
    - fixed compatibility issue in string:Split helper function

--- Helpers/string.lua
+++ Helpers/string.lua
@@ -5,11 +5,15 @@ READI.Helper.string = READI.Helper.string or {}
 
 function READI.Helper.string:Split(str)
   local words = {}
-  for word in str:gmatch("%S+") do table.insert(words, word) end
+  for word in string.gmatch(str, "%w+") do table.insert(words, word) end
   return words
 end
 
-function READI.Helper.string:Capizalize(str)
+function READI.Helper.string.Capitalize(...)
+  local size = select("#", ...)
+  local str = select(1, ...)
+  if size > 1 then str = select(2, ...) end
+
   local words = READI.Helper.string:Split(str)
   for i=1, #words do
     words[i] = words[i]:gsub("^%l", string.upper)
