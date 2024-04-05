fa192e7a5e95fbc963edc7b75eea04c67390477f
readi2play <r.dippe@rene-dippe.de>
2024-04-05 22:43:53 +0200

    * solve merge conflict

--- Helpers/string.lua
+++ Helpers/string.lua
@@ -10,17 +10,12 @@ function READI.Helper.string:GetWords(str)
   return words
 end
 
-<<<<<<< Updated upstream
-function READI.Helper.string:Capizalize(str)
-  local words = READI.Helper.string:Split(str)
-=======
 function READI.Helper.string.Capitalize(...)
   local size = select("#", ...)
   local str = select(1, ...)
   if size > 1 then str = select(2, ...) end
 
   local words = READI.Helper.string:GetWords(str)
->>>>>>> Stashed changes
   for i=1, #words do
     words[i] = words[i]:gsub("^%l", string.upper)
   end
